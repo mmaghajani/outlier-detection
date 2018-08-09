66d3a0abc32f9068f5e83af6c5e238f294d529df
mma <mma137421@gmail.com>
2018-08-09 19:12:35 +0430

    Improves DBScan for local and global file dataset

--- examples/DB_Scan.py
+++ examples/DB_Scan.py
@@ -7,21 +7,19 @@ from mlxtend.evaluate import confusion_matrix
 
 
 # 0. Data loading
-train_url = "./data_in/ipsweep_normal.csv"
+train_url = "./data_in/local.csv"
 train = pd.read_csv(train_url, delimiter=',', header=None)
 ytrain = train.iloc[:, -1]
 train = train[:-1]
 print("data is loaded")
 
-T = 7
+T = 3
 # 1. Dimension Reduction
 n = train.shape[0]
 projected = dim_red.SVD(train, T)
 
 # 2. Clustering
-# new_projected = dim_red.prepare_projected_data(projected, T)
-predict = cluster.DB_Scan(projected, 40, 2)
-print(predict)
+predict = cluster.DB_Scan(projected, 15, 5)
 index = 0
 for i in predict:
     if i < 0:
@@ -29,13 +27,17 @@ for i in predict:
     else:
         predict[index] = 0
     index += 1
-print(predict)
+
 train["predict"] = predict
 train["label"] = ytrain
 
 # 3. Evaluation
 classes = [0, 1]
 confusion_matrix_all = confusion_matrix(train["label"], train["predict"], binary=True)
+precision, recall, fscore = eval.compute_precision_recall_fscore(confusion_matrix_all)
+print("Precision : ", precision)
+print("Recall    : ", recall)
+print("FScore    : ", fscore)
 plt.figure()
 eval.plot_confusion_matrix(confusion_matrix_all, classes, normalize=False)
 plt.show()
